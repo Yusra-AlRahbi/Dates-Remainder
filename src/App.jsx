69d443e9dca051b194fc5c009388cd9fc4f10ddb
Yusra-AlRahbi <yusra5273@gmail.com>
2024-09-16 12:34:46 +0400

    Make the container design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App() {
-
   return (
     <>
       <div>
-        <h1>Hi</h1>
+        <Container className="py-5">
+          <Row className="justify-content-center">
+            <Col sm="8" className="">
+              لديك 4 مواعيد اليوم
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col sm="8" className="">
+              <div className="rectangle">
+              </div>
+            </Col>
+          </Row>
+          <Row className="justify-content-center my-2">
+            <Col sm="8" className="d-flex justify-content-between">
+             <Button className="btn-style p-2">مسح الكل</Button>
+             <Button className="btn-style p-2">عرض البيانات</Button>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
