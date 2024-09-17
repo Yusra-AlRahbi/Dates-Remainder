e8a40ff486a09dd89019a201ad24ecb2dc54dff8
Yusra-AlRahbi <yusra5273@gmail.com>
2024-09-17 11:51:19 +0400

    create data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
-
+import {data} from "./data"
 function App() {
   return (
     <>
@@ -7,26 +7,36 @@ function App() {
         <Container className="py-5">
           <Row className="justify-content-center">
             <Col sm="8" className="">
-              لديك 4 مواعيد اليوم
+              لديك {data.length} مواعيد اليوم
             </Col>
           </Row>
           <Row className="justify-content-center">
             <Col sm="8" className="">
               <div className="rectangle p-2">
-                <div className="d-flex border-bottom mx-3 my-2">
-                  <img className="img-avtar " alt="avtar" src="../src/assets/img/avatar.png"/>
-                  <div className="px-3">
-                    <p className="d-inline fs-5">يسرى زايد</p>
-                    <p className="fs-6">الخامسة مساء</p>
-                  </div>
-                </div>
+                {data.length
+                  ? data.map((item) => {
+                      return (
+                        <div key={item.id} className="d-flex border-bottom mx-3 my-2">
+                          <img
+                            className="img-avtar "
+                            alt="avtar"
+                            src="../src/assets/img/avatar.png"
+                          />
+                          <div className="px-3">
+                            <p className="d-inline fs-5">{item.name}</p>
+                            <p className="fs-6">{item.date}</p>
+                          </div>
+                        </div>
+                      );
+                    })
+                  : <h2 className="p-5 text-center">لا يوجد مواعيد اليوم</h2>}
               </div>
             </Col>
           </Row>
           <Row className="justify-content-center my-2">
             <Col sm="8" className="d-flex justify-content-between">
-             <button className="btn-style p-2">مسح الكل</button>
-             <button className="btn-style p-2">عرض البيانات</button>
+              <button className="btn-style p-2">مسح الكل</button>
+              <button className="btn-style p-2">عرض البيانات</button>
             </Col>
           </Row>
         </Container>
---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const data = [
+    { id: "1", name: "يسرى", date: "5 مساء", img: "./assets/img/avatar.png" },
+    { id: "2", name: "سلطانة", date: "1 مساء", img: "./assets/img/avatar.png" },
+    { id: "3", name: "دانة", date: "2 مساء", img: "./assets/img/avatar.png" },
+    { id: "4", name: "سارة", date: "3 مساء", img: "./assets/img/avatar.png" },
+    { id: "5", name: "هند", date: "4مساء", img: "./assets/img/avatar.png" },
+  ];
+
