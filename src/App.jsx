e3f5ca4ec526b8dce5fe0973c8202b25cdbd062d
Yusra-AlRahbi <yusra5273@gmail.com>
2024-09-17 11:26:12 +0400

    Design the data frame

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,21 @@ function App() {
           </Row>
           <Row className="justify-content-center">
             <Col sm="8" className="">
-              <div className="rectangle">
+              <div className="rectangle p-2">
+                <div className="d-flex border-bottom mx-3 my-2">
+                  <img className="img-avtar " alt="avtar" src="../src/assets/img/avatar.png"/>
+                  <div className="px-3">
+                    <p className="d-inline fs-5">يسرى زايد</p>
+                    <p className="fs-6">الخامسة مساء</p>
+                  </div>
+                </div>
               </div>
             </Col>
           </Row>
           <Row className="justify-content-center my-2">
             <Col sm="8" className="d-flex justify-content-between">
-             <Button className="btn-style p-2">مسح الكل</Button>
-             <Button className="btn-style p-2">عرض البيانات</Button>
+             <button className="btn-style p-2">مسح الكل</button>
+             <button className="btn-style p-2">عرض البيانات</button>
             </Col>
           </Row>
         </Container>
